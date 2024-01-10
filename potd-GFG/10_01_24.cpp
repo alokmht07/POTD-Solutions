a88cc4257fbe6f54a89e1b48e963eaa8c785ea3d
Alok Mahato <alokmahato861@gmail.com>
2024-01-10 10:30:54 +0530

    10/01 potd

---
+++ potd-GFG/10_01_24.cpp
@@ -0,0 +1,18 @@
+class Solution{
+public:	
+	int longSubarrWthSumDivByK(int arr[], int n, int k)
+	{
+	    // Complete the function
+	     unordered_map<int,int>mp;
+        int ans=0,sum=0;
+        mp[0]=-1;
+        for(int i=0;i<n;i++){
+            sum+=arr[i];
+            int r=sum%k;
+            if(r<0) r+=k;
+            if(mp.find(r)==mp.end()) mp[r]=i;
+            else ans=max(ans,i-mp[r]);
+        }
+        return ans;
+	}
+};
