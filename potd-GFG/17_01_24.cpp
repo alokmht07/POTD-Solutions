0204639e775d7371c8b10eec07a12b35fb82b5a2
Alok Mahato <alokmahato861@gmail.com>
2024-01-17 23:03:44 +0530

    17/01 potd

---
+++ potd-GFG/17_01_24.cpp
@@ -0,0 +1,12 @@
+class Solution {
+  public:
+    vector<vector<int>> uniquePerms(vector<int> &arr ,int n) {
+        // code here
+        vector<vector<int>>store;
+        sort(arr.begin(),arr.end());
+        do{
+            store.push_back(arr);
+        }while(next_permutation(arr.begin(),arr.end()));
+        return store;
+    }
+};
